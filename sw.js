763350abe24aae21533c1a5c3d6de0c570411a18
Veronica Cruz <vcruz9@uic.edu>
2019-04-29 04:55:27 +0000

    please work im going to cry

--- sw.js
+++ sw.js
@@ -1,52 +1,43 @@
-/*
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+version = '1.0';
 
+let cacheName = 'FluffyPleasures' + version;
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('install', e => {
+    let timeStamp = Date.now();
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `./`,
+                `./index.html`
+            ])
+                .then(() => self.skipWaiting());
+        })
+    )
 });
-
-var CACHE_NAME = 'Pics For Tears';
-var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// https://stackoverflow.com/questions/41009167/what-is-the-use-of-self-clients-claim
 
 self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request, {ignoreSearch:false}).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    // Return true to delete previous caches
+                    return true;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
